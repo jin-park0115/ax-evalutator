809b950275a409402561a35efcfe15fc4a84b2e4
안형준 <apll11@daum.net>
2026-08-14 12:15:18 +0900

    base.html최신화 및 py파일 최신화

--- project/apps/students/urls.py
+++ project/apps/students/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import student_home
+from .views import student_home, student_team
 
 urlpatterns = [
     path("", student_home, name="student_home"),
+    path("team/", student_team, name="student_team"),
 ]
--- project/apps/students/views.py
+++ project/apps/students/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def student_home(request):
     return render(request, "student/home.html")
+
+def student_team(request):
+    return render(request, "student/team.html")
