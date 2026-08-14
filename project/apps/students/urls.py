a9c8127a4cb4ef62d94abc677a62f8077a805bb2
안형준 <apll11@daum.net>
2026-08-14 11:52:01 +0900

    feat: 학생 홈 화면 추가

---
+++ project/apps/students/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import student_home
+
+urlpatterns = [
+    path("", student_home, name="student_home"),
+]
--- project/config/urls.py
+++ project/config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from apps.accounts.views import home
 
 urlpatterns = [
     path("", home, name="home"),
+    path("student/", include("apps.students.urls")),
     path("admin/", admin.site.urls),
 ]
-
