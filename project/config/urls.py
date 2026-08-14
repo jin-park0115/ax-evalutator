954750d8603fbdedde4fe858cbf1c5699c8aedb4
안형준 <apll11@daum.net>
2026-08-14 16:23:01 +0900

    feat: 로그인/로그아웃 화면 및 네비게이션 권한 분기 (#27)

--- project/config/urls.py
+++ project/config/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.http import HttpResponse
 from django.urls import path, include
 
@@ -13,5 +14,8 @@ urlpatterns = [
     path("", home, name="home"),
     path("student/", include("apps.students.urls")),
     path("tutor/rounds/", tutor_rounds, name="tutor_rounds"),
+    path("accounts/login/", auth_views.LoginView.as_view(
+        template_name="accounts/login.html"), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("admin/", admin.site.urls),
 ]
