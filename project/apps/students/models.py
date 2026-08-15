6db3a07535a53afef9df497e4f4801e6cf590f29
전예진 <wjs0951467@gmail.com>
2026-08-15 23:01:54 +0900

    ERD 및 평가 기능 수정

--- project/apps/students/models.py
+++ project/apps/students/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Student(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="student",
+    )
     name = models.CharField(max_length=100)
     email = models.EmailField(blank=True, unique=True)
 
     def __str__(self) -> str:
         return self.name
-
